01885bb9e909ae1185e2cab280f76242f645cf59
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-25 10:43:59 +0300

    refactor: change create todo folder to read todo

--- app/read_todo/page.tsx
+++ app/read_todo/page.tsx
@@ -2,7 +2,7 @@
 
 import { createClient } from "@/utils/supabase/server";
 
-export default async function create_todo(){
+export default async function read_todo(){
     const supabase = createClient();
     let { data: TODO } = await supabase.from('TODO').select('*')
     return <div>{JSON.stringify(TODO)}</div>
